66c95727aabf10f15318d2686f8a5e671309c56d
Hoang Nguyen <hoangmegamind1302@gmail.com>
2024-04-14 03:29:42 +0700

    create solution and srcSolution function

---
+++ src/models/solution.model.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const Solution = function(sol) {
+    this.solutionid = sol.solutionid,
+    this.content = sol.content, 
+    this.patientid = sol.patientid, 
+    this.problemid = sol.problemid
+}
+
+Solution.create = async(newSol) => {
+    //Generate a random UUID (v4)
+    const uuid = crypto.randomUUID();
+
+    //Hash the UUID with MD5
+    const md5hash = crypto.createHash('md5').update(uuid).digest('hex');
+
+    //Use md5hash as the userid in newUser
+    newSol.solutionid = md5hash;
+
+    try {
+        const createdSolution = await prisma.create({
+            data: {
+                solutionid: newSol.solutionid, 
+                content: newSol.content, 
+                patientid: newSol.patientid, 
+                problemid: newSol.problemid
+            }
+        });
+    } catch(error) {
+        console.error("Error creating solution: ", error);
+        throw error;
+    }
+};
--- src/models/srcSolution.model.js
+++ src/models/srcSolution.model.js
@@ -1,1 +1,33 @@
-    
+const crypto = require('crypto');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const srcSolution = function(srcSol) {
+    this.id = srcSol.id, 
+    this.content = srcSol.content, 
+    this.problem = srcSol.problem
+}
+
+srcSolution.create = async(newSrcSol) => {
+    //Generate a random UUID (v4)
+    const uuid = crypto.randomUUID();
+
+    //Hash the UUID with MD5
+    const md5hash = crypto.createHash('md5').update(uuid).digest('hex');
+
+    //Use md5hash as the userid in newUser
+    newSrcSol.solutionid = md5hash;
+
+    try {
+        const createdSrcSol = await prisma.create({
+            data: {
+                id: newSrcSol.id, 
+                content: newSrcSol.content, 
+                problem: newSrcSol.problem
+            }
+        });
+    } catch(error) {
+        console.error("Error creating src: ", error);
+        throw error;
+    }
+};
