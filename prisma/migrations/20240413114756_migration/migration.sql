61e97d3d0f18a2edf1930760300d20203d72ccc0
Hoang Nguyen <hoangmegamind1302@gmail.com>
2024-04-13 18:48:57 +0700

    add Entities

---
+++ prisma/migrations/20240413114756_migration/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `userid` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` INTEGER NOT NULL DEFAULT 1,
+    `country` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_phone_key`(`phone`),
+    PRIMARY KEY (`userid`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Incident` (
+    `incidentid` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `dateOccured` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `damage` INTEGER NOT NULL,
+    `patientid` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`incidentid`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Solution` (
+    `solutionid` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `patientid` VARCHAR(191) NOT NULL,
+    `problemid` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`solutionid`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `srcSolution` (
+    `id` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `problem` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Incident` ADD CONSTRAINT `Incident_patientid_fkey` FOREIGN KEY (`patientid`) REFERENCES `User`(`userid`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Solution` ADD CONSTRAINT `Solution_patientid_fkey` FOREIGN KEY (`patientid`) REFERENCES `User`(`userid`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Solution` ADD CONSTRAINT `Solution_problemid_fkey` FOREIGN KEY (`problemid`) REFERENCES `Incident`(`incidentid`) ON DELETE RESTRICT ON UPDATE CASCADE;
